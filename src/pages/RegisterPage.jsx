d6178e2264ad9944a021c15a5dbc5effb0da1a65
Alina <logunionok@gmail.com>
2023-04-23 23:59:25 +0300

    fix: add request for login and register page

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Button, Footer, Header, Input } from '../components';
 import { Link } from 'react-router-dom';
+import { UserService } from '../services/network';
 
 const RegisterPage = () => {
   const [email, setEmail] = useState('');
@@ -8,12 +9,24 @@ const RegisterPage = () => {
   const [name, setName] = useState('');
   const [surname, setSurname] = useState('');
 
+  const register = async (email, password, name, surname) => {
+    try {
+      console.log('PRESSED: ', process.env.NODE_ENV);
+      await UserService.register(email, password, name, surname);
+    } catch (error) {
+      console.log('ERROR: ', error);
+      // errorHandler(error)
+    }
+  };
+
   return (
     <div>
       <Header />
       <main>
         <div className="form__container">
-          <p className="title">Добро пожаловать, тут ты можешь создать новый аккаунт!</p>
+          <p className="title">
+            Добро пожаловать, тут ты можешь создать новый аккаунт!
+          </p>
           <Input
             placeholder="Имя"
             setValue={(e) => setName(e.target.value)}
@@ -38,7 +51,11 @@ const RegisterPage = () => {
             value={password}
             type="password"
           />
-          <Button text="Создать" style="mainBtn" />
+          <Button
+            text="Создать"
+            style="mainBtn"
+            onPress={() => register(email, password, name, surname)}
+          />
           <p className="paragraph-small">У тебя уже есть аккаунт?</p>
           <Link to="/login" style={{ color: 'black', padding: '4px' }}>
             <p className="paragraph-small">Войди здесь</p>
--- src/services/network/user.js
+++ src/services/network/user.js
@@ -7,7 +7,14 @@ export class UserService {
     login: '/user/login',
   };
 
-  static async register(email = '', password = '', name = '', surname = '') {
+  static async register(
+    email = '',
+    password = '',
+    name = '',
+    surname = '',
+    // id = null,
+    // token = null,
+  ) {
     try {
       const data = await api.post(`${BASE_API}${this.endpoints.register}`, {
         body: JSON.stringify({ email, password, name, surname }),
@@ -16,6 +23,19 @@ export class UserService {
       log('api-success', '[USER] Register', data);
     } catch (error) {
       log('api-error', '[USER] Register: ', error);
+      throw error;
+    }
+  }
+
+  static async login(email = '', password = '') {
+    try {
+      const data = await api.post(`${BASE_API}${this.endpoints.login}`, {
+        body: JSON.stringify({ email, password }),
+      });
+
+      log('api-success', '[USER] Login', data);
+    } catch (error) {
+      log('api-error', '[USER] Login: ', error);
       throw error;
     }
   }
